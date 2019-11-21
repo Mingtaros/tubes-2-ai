6700478e0002519cdad69dc7f801a279ce32c0fd
Leonardo W <13517048@std.stei.itb.ac.id>
2019-11-21 08:23:27 +0700

    Initialized Rule-Based Shape Detection

--- DetectShape.py
+++ DetectShape.py
@@ -1,36 +1,47 @@
 import cv2
-import numpy as np
+import Rules
 
-font = cv2.FONT_HERSHEY_COMPLEX
+def findShapes(filename, shape): # diasumsikan filename sudah ditambahkan "images/" diawal
+    font = cv2.FONT_HERSHEY_COMPLEX
 
-filename = "images/" + input("Filename: ")
-img = cv2.imread(filename)
+    img = cv2.imread(filename)
 
-cv2.imshow("initial", img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, threshold = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-_, threshold = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
-contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # print(contours)
 
-print(contours)
+    for cnt in contours:
+        approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
+        shape_detector = Rules.ShapeIdentifier()
+        shape_detector.reset()
+        shape_detector.result = []
+        shape_detector.declare(Rules.Fact(jumlah_sisi = len(approx)))
+        shape_detector.run()
+        if (shape in shape_detector.result):
+            # x dan y merupakan x dan y letak shape
+            x = approx.ravel()[0]
+            y = approx.ravel()[1]
+            cv2.drawContours(img, [approx], 0, (0), 3)
+            cv2.putText(img, shape, (x,y), font, 1, (0))
 
-for cnt in contours:
-    approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
-    cv2.drawContours(img, [approx], 0, (0), 5)
-    # x dan y merupakan x dan y letak shape
-    x = approx.ravel()[0]
-    y = approx.ravel()[1]
-    if len(approx) == 3:
-        cv2.putText(img, "Triangle", (x, y), font, 1, (0))
-    elif len(approx) == 4:
-        cv2.putText(img, "Rectangle", (x, y), font, 1, (0))
-    elif len(approx) == 5:
-        cv2.putText(img, "Pentagon", (x, y), font, 1, (0))
-    elif 6 < len(approx) < 15:
-        cv2.putText(img, "Ellipse", (x, y), font, 1, (0))
-    else:
-        cv2.putText(img, "Circle", (x, y), font, 1, (0))
+        # if len(approx) == 3:
+        #     cv2.putText(img, "Triangle", (x, y), font, 1, (0))
+        # elif len(approx) == 4:
+        #     cv2.putText(img, "Rectangle", (x, y), font, 1, (0))
+        # elif len(approx) == 5:
+        #     cv2.putText(img, "Pentagon", (x, y), font, 1, (0))
+        # elif 6 < len(approx) < 15:
+        #     cv2.putText(img, "Ellipse", (x, y), font, 1, (0))
+        # else:
+        #     cv2.putText(img, "Circle", (x, y), font, 1, (0))
 
-cv2.imshow("shapes", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    return img
+
+if __name__ == "__main__":
+    filename = "images/" + input("Filename: ")
+    shape = input("Shape to search for: ")
+    after = findShapes(filename, shape)
+    cv2.imshow("after", after)
+    cv2.waitKey(0) # quit with pressing 0
--- Rules.py
+++ Rules.py
@@ -1,38 +1,32 @@
 from experta import *
-from random import choice
+import numpy as np
 
 class ShapeIdentifier(KnowledgeEngine):
-    @Rule(Fact(color = "green", kaki = True))
-    def green_Fact(self):
-        print("walk")
-    
-    @Rule(Fact(color = "green", kaki = True))
-    def red_Fact(self):
-        print("stop")
-    
-    @Rule(AS.Fact << Fact(color = "yellow", kaki = False))
-    def cautious(self, Fact):
-        print("Be cautious because light is", Fact["color"])
+    result = []
 
-    @Rule(Fact(color = "yellow"))
-    def itsyellow(self):
-        print("ya iya itu")
+    @Rule(Fact(jumlah_sisi = 3))
+    def segitiga(self):
+        self.result.append("Segitiga")
     
-    @Rule(AS.Fact << Fact(color = "red", kaki = False))
-    def cautious_banget(self, Fact):
-        self.cautious(Fact)
+    @Rule(Fact(jumlah_sisi = 4))
+    def segiempat(self):
+        self.result.append("Segi Empat")
+        
+    @Rule(Fact(jumlah_sisi = 5))
+    def segilima(self):
+        self.result.append("Segi Lima")
+
+    @Rule(Fact(jumlah_sisi = 6))
+    def segienam(self):
+        self.result.append("Segi Enam")
 
 if __name__ == "__main__":
-    engine = ShapeIdentifier()
     while (True):
-        color = input("color: ")
-        kaki = bool(input("kaki: ")) #klo mau resultnya false, langsung enter aja
-        engine.reset()
-        engine.declare(Fact(color = color, kaki = kaki))
-        engine.run()
-
-# bisa ngetrigger lebih dari 1 rule
-# bisa lebih dari 1 fakta dalam 1 rule ==> Fact(f1 = d1, f2 = d2, f3 = d3, ..., fn = dn) (suatu and)
-# bisa juga pake rule lain yang memanggil fungsi yang ditrigger rule lain (suatu or)
-# bisa dipanggil terus-terusan
-# AS.Fact biar Fact bisa dipanggil sebagai suatu parameter di fungsinya
+        shape_detector = ShapeIdentifier()
+        shape_detector.reset()
+        shape_detector.result = [] #reset
+        coordinates = list(input("Koordinat: "))
+        shape_detector.declare(Fact(jumlah_sisi = len(coordinates)))
+        shape_detector.run()
+        print(shape_detector.result)
+        
